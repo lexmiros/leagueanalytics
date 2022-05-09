69e46009952fc0c824380d154698a130b154e920
Lex Miros <aleksei.miros@outlook.com>
2022-05-09 16:08:52 +1000

    Daily API update

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 
-api_key = 'RGAPI-11a82859-0d57-4d16-aaaf-dfb7c6c77edb'
+api_key = 'RGAPI-37c3cc3a-7247-4626-b5b6-693a609a76af'
 watcher = LolWatcher(api_key)
 my_region = 'OC1'
 
