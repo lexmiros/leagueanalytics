5060c837c90aaaac8ccc6a0f03c178f8e6ba8823
Lex Miros <aleksei.miros@outlook.com>
2022-05-09 19:24:08 +1000

    Created function to conver a df column to type str

--- src/CleanData.py
+++ src/CleanData.py
@@ -2,13 +2,17 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
-def encode_winloss(x):
-    if x == True:
+def encode_true_false(df_column):
+    if df_column == 'True':
         return 1
-    elif x == False:
+    elif df_column == 'False':
         return 0 
     else:
         return "Error"
+
+def col_to_string(df, col_name):
+    df[col_name] = df[col_name].astype(str)
+    return df
 
 def encode_categorical(df, col_name):
     encoded = pd.get_dummies(df[col_name])
@@ -20,6 +24,7 @@ if __name__ == "__main__":
 
     df = pd.read_csv('./TestData')
     df = pd.DataFrame(df)
+    df = col_to_string(df, "WinLoss")
     df['WinLoss'] = df['WinLoss'].map(encode_winloss)
 
     df = encode_categorical(df, "Lane")
