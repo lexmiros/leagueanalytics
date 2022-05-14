f6f231a2e0c57bd3ced5d3fd9b3f33cd2b589666
Lex Miros <aleksei.miros@outlook.com>
2022-05-14 12:59:28 +1000

    Even more html / css styling

--- src/flaskApp/routes.py
+++ src/flaskApp/routes.py
@@ -11,7 +11,7 @@ def home():
     
     return render_template("landingPage.html")
 
-@app.route("/graph")
+@app.route("/overview")
 def graph():
     n = 10
     top_wr_champs = top_champs_by_wr(df, n)
@@ -21,4 +21,4 @@ def graph():
         labels.append(champs["Name"])
         win_rates.append(champs["Win Rate"])
 
-    return render_template("graph_temp.html", labels = labels, win_rates = win_rates, n = n)
+    return render_template("overview.html", labels = labels, win_rates = win_rates, n = n)
