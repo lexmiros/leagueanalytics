6bfa133f1621f34b6330a9cb5db6be81168dad72
Lex Miros <aleksei.miros@outlook.com>
2022-05-24 17:50:40 +1000

    Update API key and CSS

--- src/DataScripts/__init__.py
+++ src/DataScripts/__init__.py
@@ -1,4 +1,4 @@
 from riotwatcher import LolWatcher, ApiError
 
-api_key = "RGAPI-c6d42f45-25f8-474b-b626-a2278df23bc6"
+api_key = "RGAPI-251cfe28-27c6-4b81-b44c-4bbc914504d9"
 watcher = LolWatcher(api_key)
