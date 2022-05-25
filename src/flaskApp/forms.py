6165eb1ffc69b3546c2cf73db28867f9a7ce73f4
Lex Miros <aleksei.miros@outlook.com>
2022-05-25 12:53:08 +1000

    Edited input box text

--- src/flaskApp/forms.py
+++ src/flaskApp/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 
 class UserNameForm(FlaskForm):
-    user = StringField("Username...", render_kw={"Placeholder" : "Submit EMPTY search for test data or input summoner name"})
+    user = StringField("Username...", render_kw={"Placeholder" : "Summoner name..."})
     region = SelectField("Region", choices=[('OC1', 'OCE'), ('NA1', 'NA'), ('BR', 'BR'), ('EUN1', 'EUN'), ('EUW1', 'EUW')
         , ('JP1', 'JP'), ('KR', 'KR'), ('LA1', 'LA1'), ('LA2', 'LA2'), ('TR1', 'TR'), ('RU', 'RU')],
         validators=[DataRequired()])
